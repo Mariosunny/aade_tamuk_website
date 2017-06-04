04ecf5a112372a7690a4e8e4591451d1aeb85d59
Chris <ranger2242@gmail.com>
2017-06-04 17:21:50 -0500

    Added test framework for struts2 in intellij

---
+++ server2/StrutsTest/src/actions/TestAction.java
@@ -0,0 +1,11 @@
+package actions;
+
+/**
+ * Created by Chris Cavazos on 5/28/2017.
+ */
+public class TestAction {
+    public String execute(){
+        System.out.println("What");
+        return "test";
+    }
+}
