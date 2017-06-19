299e3d6dc3fb3574f90fbab92fa0d9ac29853ea3
Tyler Hurson <glassplotful@gmail.com>
2017-06-19 17:37:23 -0500

    Added hibernate test application.

---
+++ hibernate2/src/main/Employee.java
@@ -0,0 +1,34 @@
+package main;
+
+/**
+ * Created by tyler on 6/17/17.
+ */
+public class Employee {
+
+    private int id;
+    private String firstName, lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
